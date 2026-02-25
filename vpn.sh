81832d93916bee76225c466ac057c1a6307f80dc
Esteban Zárate <estebanzarateok@gmail.com>
2026-02-25 10:13:47 -0300

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-
-ip_address=$(/bin/cat $HOME/.config/polybar/scripts/target.txt)
-
-if [ -n "$ip_address" ]; then
-  echo "$ip_address"
-else
-  echo ""
-fi
-[melvin@archlinux scripts]$ cat vpn.sh 
 #!/bin/sh
 
 IFACE=$(ip -o link show | awk -F': ' '/tun0/ {print $2}')
