5b547896bed7ed94b42522b69c5a112f4a0d5ffc
Esteban Zárate <estebanzarateok@gmail.com>
2026-02-28 15:55:01 -0300

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,7 @@
+source $HOME/.config/colors/colors.sh
+
+export MAIN_DOG_LABEL="[%{F$COLOR_DOG} 󰩃  %{F-}]"
+export MAIN_FIRE_LABEL="(%{F$COLOR_DANGER} F %{F-})"
+export MAIN_BURP_LABEL="(%{F$COLOR_ORANGE} B %{F-})"
+export MAIN_WIRE_LABEL="(%{F$COLOR_PRIMARY} W %{F-})"
+export MAIN_TOR_LABEL="(%{F#7952B3} T %{F-})"
